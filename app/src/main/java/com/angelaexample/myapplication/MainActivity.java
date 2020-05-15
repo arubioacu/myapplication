3c8776210152b3ab2879e4809cb4f30ab5ed48e4
arubioacu <angela.rubio@mail.udp.cl>
2020-05-15 00:04:20 -0400

    primer commit incio proyecto uno app programacion !

--- app/src/main/java/com/angelaexample/myapplication/MainActivity.java
+++ app/src/main/java/com/angelaexample/myapplication/MainActivity.java
@@ -2,13 +2,47 @@ package com.angelaexample.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.Toast;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private ImageView ball;
+    private TextView
+
+
+    private String[] respuestaArray = {"Es cierto", "Definitivamente es así", "Sin duda", "Sí definitivamente", "Puede confiar en él", "Como yo lo veo, sí",
+            "Lo más probable", "Perspectiva buena", "Sí", "Las señales apuntan a sí", "Respuesta confusa intente de nuevo", "Pregunte de nuevo más tarde",
+            "Mejor no decirte ahora", "No puedo predecir ahora", "Concéntrate y pregunta de nuevo", "No cuentes con eso",
+            "Mi respuesta es no", "Mis fuentes dicen que no", "Outlook no es tan bueno", "Muy dudoso"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //
+        ball =findViewById(R.id.boton);
+        //
+        respuestaArray = findViewById(R.id.respuesta);
+
+        ball.setOnClickListener(this);
+
+        Toast.makeText( contextMainActivity.this, text: "Descubre Tu Destino Magic Ball", Toast.LENGTH_SHORT).show();
     }
+
+    private void show() {
+    }
+
+    @Override
+
+    public void onClick(View v){...}
+
 }
+
+
